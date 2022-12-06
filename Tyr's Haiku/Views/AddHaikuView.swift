65549dc3149281796c1bd753a459774c85c3805d
Tyr Zong <tianyizong@gmail.com>
2022-12-05 21:11:24 -0500

    added app icons and next field functionality

--- Tyr's Haiku/Views/AddHaikuView.swift
+++ Tyr's Haiku/Views/AddHaikuView.swift
@@ -10,21 +10,80 @@ import SwiftUI
 struct AddHaikuView: View {
     @Environment(\.managedObjectContext) var managedObjContext
     @Environment(\.dismiss) var dismiss
-    
+    private let pasteBoard = UIPasteboard.general
+   
+    enum Field{
+        case line1, line2, line3, title
+    }
     @State private var line1 = ""
     @State private var line2 = ""
     @State private var line3 = ""
     @State private var title = ""
+    @FocusState private var focusField: Field?
+    
+    
+    private func formatDateString(date: Date) -> String{
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .none
+        return formatter.string(from: date)
+    }
+    
+    private func copyHaiku(){
+        pasteBoard.string = formatDateString(date: Date()) + "\n" + line1 + "\n" + line2 + "\n" + line3 + "\n— inspired by " + title
+    }
     
     var body: some View {
+
         Form{
             Section{
                 TextField("First line", text:$line1)
+                    .submitLabel(.next)
+                    .focused($focusField, equals: .line1)
+                    .onSubmit{
+                        focusField = .line2
+                    }
                 TextField("Second line", text:$line2)
+                    .submitLabel(.next)
+                    .focused($focusField, equals: .line2)
+                    .onSubmit{
+                        focusField = .line3
+                    }
                 TextField("Third line", text:$line3)
+                    .submitLabel(.next)
+                    .focused($focusField, equals: .line3)
+                    .onSubmit{
+                        focusField = .title
+                    }
                 HStack{
                     Text("Inspired by:").foregroundColor(.gray)
                     TextField("", text: $title)
+                        .submitLabel(.done)
+                        .focused($focusField, equals: .title)
+                        .onSubmit {
+                            focusField = nil
+                        }
+                }
+            }
+            Section{
+                HStack{
+                    Spacer()
+                    Button("Clear"){
+                        line1 = ""
+                        line2 = ""
+                        line3 = ""
+                        title = ""
+                    }
+                    Spacer()
+                }
+            }
+            Section{
+                HStack{
+                    Spacer()
+                    Button("Copy"){
+                        copyHaiku()
+                    }
+                    Spacer()
                 }
             }
             HStack{
@@ -37,6 +96,7 @@ struct AddHaikuView: View {
             }
         }
     }
+    
 }
 
 struct AddHaikuView_Previews: PreviewProvider {
--- Tyr's Haiku/Views/EditHaikuView.swift
+++ Tyr's Haiku/Views/EditHaikuView.swift
@@ -10,13 +10,31 @@ import SwiftUI
 struct EditHaikuView: View {
     @Environment(\.managedObjectContext) var managedObjContext
     @Environment(\.dismiss) var dismiss
+    private let pasteBoard = UIPasteboard.general
     
     var haiku: FetchedResults<Haiku>.Element
+    
+    enum Field{
+        case line1, line2, line3, title
+    }
     
     @State private var line1 = ""
     @State private var line2 = ""
     @State private var line3 = ""
     @State private var title = ""
+    @FocusState private var focusField: Field?
+    
+    
+    private func formatDateString(date: Date) -> String{
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .none
+        return formatter.string(from: date)
+    }
+    
+    private func copyHaiku(){
+        pasteBoard.string = formatDateString(date: Date()) + "\n" + line1 + "\n" + line2 + "\n" + line3 + "\n— inspired by " + title
+    }
     
     var body: some View {
         Form{
@@ -28,11 +46,52 @@ struct EditHaikuView: View {
                         line3 = haiku.line3!
                         title = haiku.title!
                     }
+                    .submitLabel(.next)
+                    .focused($focusField, equals: .line1)
+                    .onSubmit{
+                        focusField = .line2
+                    }
                 TextField("\(haiku.line2!)", text:$line2)
+                    .submitLabel(.next)
+                    .focused($focusField, equals: .line2)
+                    .onSubmit{
+                        focusField = .line3
+                    }
                 TextField("\(haiku.line3!)", text:$line3)
+                    .submitLabel(.next)
+                    .focused($focusField, equals: .line3)
+                    .onSubmit{
+                        focusField = .title
+                    }
                 HStack{
                     Text("Inspired by:").foregroundColor(.gray)
                     TextField("\(haiku.title!)", text: $title)
+                        .submitLabel(.done)
+                        .focused($focusField, equals: .title)
+                        .onSubmit {
+                            focusField = nil
+                        }
+                }
+            }
+            Section{
+                HStack{
+                    Spacer()
+                    Button("Clear"){
+                        line1 = ""
+                        line2 = ""
+                        line3 = ""
+                        title = ""
+                    }
+                    Spacer()
+                }
+            }
+            Section{
+                HStack{
+                    Spacer()
+                    Button("Copy"){
+                        copyHaiku()
+                    }
+                    Spacer()
                 }
             }
             HStack{
